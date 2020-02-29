b84a1e412c833b775529e9c32fbe9dfb70ceddea
aramdass <56938424+aramdass@users.noreply.github.com>
2020-02-29 00:08:13 -0500

    Update and rename It's Laboy.py to CSCI 127 Lab Access Program.py

--- CSCI 127 Lab Access Program.py
+++ CSCI 127 Lab Access Program.py
@@ -1,14 +1,14 @@
-# Alec Ramdass
-# Tuesday, November 12th, 2019
 '''
-Description:
-This program will open whichever Lab you need
-for CSCI-127 according to the number you input
+Author: Alec Ramdass
+Date: Tuesday, November 12th, 2019
+
+Description: This program was written to conveniently 
+access the labs for my CSCI-127 class.
 '''
+import sys as s
+import webbrowser as w
 
-import webbrowser
-
-print('Use this program to Open any Lab you need to review for CSCI-127\n')
+print('To open a labs from CSCI-127 (Introduction to Computer Science)')
 
 Lab1 = "https://stjohn.github.io/teaching/csci127/f19/lab1.html"
 Lab2 = "https://stjohn.github.io/teaching/csci127/f19/lab2.html"
@@ -22,9 +22,14 @@ Lab9 = "https://stjohn.github.io/teaching/csci127/f19/lab9.html"
 Lab10 = "https://stjohn.github.io/teaching/csci127/f19/lab10.html"
 Lab11 = "https://stjohn.github.io/teaching/csci127/f19/lab11.html"
 
-site = ('ERROR',Lab1,Lab2,Lab3,Lab4,Lab5,Lab6,Lab7,Lab8,Lab9,Lab10,Lab11)
+site = (0,Lab1,Lab2,Lab3,Lab4,Lab5,Lab6,Lab7,Lab8,Lab9,Lab10,Lab11)
 
 while True:
-    LabNum = int(input('Enter Desired Lab Number: '))
-    webbrowser.open(site[LabNum])
+    labNum = int(input("[Type '0' to exit program]\nEnter Lab Number(1-11): "))
+    if type(labNum) != type(0):
+        print("Enter a number between 1 and 11")
+    elif labNum == 0:
+       s.exit()
+    else:
+        w.open(site[labNum])
 
